74ee78268c4a4e92d431a3baddfe5b1d11ecec21
Dev1L14 <yanis.timirchev@yandex.ru>
2022-10-20 17:41:59 +0300

    Fix if no solutions

--- Lab2/Lab2_8.c
+++ Lab2/Lab2_8.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <math.h>
-#define EPSILON 0.0000000001
 
 double f(double x) {
     return x*x - 5 * (sin(x));
 }
 
-double dichotomy(double a, double b){
+double dichotomy(double a, double b, int n){
+    double EPSILON = pow(10,-n);
     if((b-a)<EPSILON){
         a = a + b;
         b = a - b;
         a = a-b;
     }
-    double c = 0.0;
+    double c = (a + b) / 2.0;
+    if(fabs(f(c))<EPSILON){
+        return c;
+    }
     do{
-        c = (a + b) / 2.0;
         if(f(c) * f(a) < EPSILON) b = c;
-        else a = c;
+        else if(f(c) * f(b) < EPSILON) a = c;
+        else{
+            return NAN;
+        }
+        c = (a + b) / 2.0;
     }
     while(fabs(a-b) >= EPSILON);
     return c;
 }
 
 int main() {
-    printf("\nX: %lf\n", dichotomy((double)2.0, (double)10.0));
+    double result = dichotomy((double)2.0, (double)10.0, 10);
+    if(!isnan(result)) printf("\nSolution for x in given interval is %lf\n", result);
+    else printf("No solutions in given interval");
 }
